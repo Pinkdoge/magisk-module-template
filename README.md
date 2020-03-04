ffc00ef41ca725b23045b9e55e9244863a8b4e0d
Pinkdoge <3361534564@qq.com>
2020-03-04 21:14:36 +0800

    fix & add some tips

--- README.md
+++ README.md
@@ -14,10 +14,12 @@ module.zip
 │
 ├── customize.sh                       <---  这个文件包含 "SKIPUNZIP=0" 如需更改权限 请把0改为1 
 │                                           这个文件由 update-binary 执行(sourced)
-├── ...
+├── module.prop
 ├── ...  /* 模块文件的其余部分 */
 |
 ```
+**这意味着除了以上重要文件 其他的文件如果您不需要的话 可以删掉**
+
 **这是 [Magisk 官方新模块模板](https://github.com/HANA-CI-Build-Project/magisk-module-template) 您也可以去看看**
 
 **[Magisk 面具新版模板模块制作教程](https://www.coolapk.com/feed/16056941?shareKey=YWI0MDFiYWE1Y2E3NWUyYzA3ODc~&shareUid=1124169&shareFrom=com.coolapk.market_10.0.1) 感谢@碎念**
--- customize.sh
+++ customize.sh
@@ -1,2 +1,22 @@
-# 注意 这不是占位符！！修改权限的时候请把0改为1
+# 注意 这不是占位符！！这个代码的作用是将模块里的东西全部塞系统里，然后挂上默认权限
 SKIPUNZIP=0
+
+# 删除文件
+REPLACE="
+# 以下是一些例子
+# /system/app/Youtube
+# /system/priv-app/SystemUI
+# /system/priv-app/Settings
+# /system/framework
+"
+
+# 设置权限
+set_permissions() {
+  # 以下是一些例子:
+  # set_perm_recursive  $MODPATH/system/lib       0     0       0755      0644
+  # set_perm  $MODPATH/system/bin/app_process32   0     2000    0755      u:object_r:zygote_exec:s0
+  # set_perm  $MODPATH/system/bin/dex2oat         0     2000    0755      u:object_r:dex2oat_exec:s0
+  # set_perm  $MODPATH/system/lib/libart.so       0     0       0644
+}
+
+# 如果您还需要更多自定义设置 请自行设置代码
